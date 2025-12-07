05c836e54d4fec353ea673e968d4a70c519d5122
Sroopesh1122 <shivuroopesh6362@gmail.com>
2025-12-07 09:54:30 +0530

    Update Home component with blank page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1153,3 +1153,14 @@
 // };
 
 // export default Home;
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">Under Development</h1>
+    </div>
+  );
+};
+
+export default Home;
